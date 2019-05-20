2bfad991bca1ca9548fce06564292960b08a73d0
NickWolfen99 <strftwolfen99@gmail.com>
2019-05-20 22:05:36 +0300

    Initial commit

--- brackets/Program.cs
+++ brackets/Program.cs
@@ -10,6 +10,22 @@ namespace brackets
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter your string:");
+            string text = Console.ReadLine();
+            int n = text.Length;
+
+            int openingbrackets = 0;
+            int closingbrackets = 0;
+
+            for (int i = 0; i < n; i++)
+            { char a = text[i];
+                if (a == '(' && openingbrackets>=closingbrackets) openingbrackets++;
+                if (a == ')') closingbrackets++;
+            }
+            
+            if (openingbrackets == closingbrackets) Console.WriteLine("The parentheses (round brackets) are placed right");
+            else Console.WriteLine("The parentheses (round brackets) are placed wrong");
+            Console.ReadLine();
         }
     }
 }
